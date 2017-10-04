4c44c3b40d26bc96e7ab66e18c1bdc6a0792aaef
Alex C Fleming <alex.c.fleming@gmail.com>
2017-10-04 18:45:55 -0500

    Assignment Complete
    Complete submission

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# JHUDS_GCD_Wk4_FinalProj
 # Final Project for 'Getting and Cleaning Data' Course at JHU Coursera
+## Data Science Specialization Program
 
 by Alex C. Fleming (alex.c.fleming@gmail.com)
+<https://github.com/alexcfleming/>
+
+Folder Name:JHUDS_GCD_Wk4_FinalProj
 
 The dataset for this project is used with the following citation:
 [1] Davide Anguita, Alessandro Ghio, Luca Oneto, Xavier Parra and Jorge L. Reyes-Ortiz. Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine. International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
 This dataset is distributed AS-IS and no responsibility implied or explicit can be addressed to the authors or their institutions for its use or misuse. Any commercial use is prohibited.
 Jorge L. Reyes-Ortiz, Alessandro Ghio, Luca Oneto, Davide Anguita. November 2012.
 
+This project involves the ingesting data from a training and test set that measured the velocity and acceleration of 30 people wearing a smart phone. Each of the 30 people was recorded performing 6 different activities, and the raw data was sampled and then summarized in several different ways to produce a 561-feature vector for each record. There are different index files that link the 30 people to their records and the records to the activity being performed. 
+
+The data from 21 subjects was used in the training set. The data from the remaining 9 subjects was used in the test set. Both the training and test sets represent all 6 activities. Each subject has a slightly different number of observations (between 288 (subject 9) and 409 (subject 25)).
+
+This data set was intended to be used to train algorithms that recognize human activities from accelerometer and gyroscope signals, but we are using it as a test case to perform data manipulations.
+
+The working folder contains the raw data (rawdata) and a folder with the output products of run_analysis.R which are two tidy datasets (tidydata). The output tidy data is written in CSV format.
+
+The final tidy data sets include:
+* One set (tidydataFull) with 82 variables and 10299 observations (all feature vectors for all observations)
+* One set (tidydataAverages) with 82 variables and 180 observations (an average for each variable for each of the 6 activities for each of the 30 subjects)
+
+** IMPORTANT NOTE: The analysis function assumed that the working directory has been set to the folder that contains this README, the CodeBook, and the rawdata/tidydata folders **
--- run_analysis.R
+++ run_analysis.R
@@ -1,20 +1,54 @@
-run_analysis <- function(inputdata, ...){
+run_analysis <- function(...){
      #This will run the analysis that cleans and merges
      #the training and test sets to produce a tidy data set
+     #this function assumes the working directory has been set
+     #to the folder structure that contains the rawdata and tidydata folders
      library(dplyr)
-     library(data.table)
      library(tidyr)
      library(readr)
-     library(lubridate)
-     library(Hmisc)
-     #Initialize dataframe
-     inputdata <- data.table(read.csv())
-     #Load the data sets and convert to data frames
-     #Adjust names and data formats for cross index
-     #Merge and join output classifiers with input data
-     #Stack training and test datasets into one
-     #Conduct any transformations to make tidy
-     #Adjust final names, variable definitions, and labels
-     #Output the final data file into CSV and return
-     return(tidydata)
+     library(data.table)
+     #Initialize data tables and load data
+     X_train <- fread("./rawdata/train/X_train.txt")
+     y_train <- fread("./rawdata/train/y_train.txt")
+     subject_train <- fread("./rawdata/train/subject_train.txt")
+     X_test <- fread("./rawdata/test/X_test.txt")
+     y_test <- fread("./rawdata/test/Y_test.txt")
+     subject_test <- fread("./rawdata/test/subject_test.txt")
+     features <- fread("./rawdata/features.txt")
+     activity_labels <- fread("./rawdata/activity_labels.txt")
+     #Stack subject, training and test sets into one combined X and Y
+     X <- bind_rows(X_train,X_test)
+     y <- bind_rows(y_train,y_test)
+     subject <- bind_rows(subject_train, subject_test)
+     SubjectXY <- bind_cols(subject,X,y)
+     #Sets names for certain columns in combined table
+     setnames(SubjectXY, c("V1","V1100","V1101"),c("SubjectNum","V1","ActivityCode"))
+     #Apply activity descriptions to Y index values as labels in new column
+     setnames(activity_labels, c("V1","V2"), c("ActivityCode","ActivityDesc"))
+     SubjectXYlabeled <- left_join(SubjectXY, activity_labels)
+     #Remove Feature Columns to leave the mean and SD of each measurement
+     meanfeatures <- features[V2 %like% "mean"]
+     stdfeatures <- features[V2 %like% "std"]
+     filteredfeatures <- bind_rows(meanfeatures, stdfeatures)
+     orderedfeatures <- arrange(filteredfeatures, V1)
+     orderedfeaturesDT <- data.table(orderedfeatures)
+     orderedfeaturesDT[,V1:=sub("^","V", orderedfeaturesDT$V1)]
+     keepindex <- orderedfeaturesDT$V1
+     keepindex <- c(keepindex,"SubjectNum","ActivityCode","ActivityDesc")
+     keep.bool <- names(SubjectXYlabeled) %in% keepindex
+     SubjectXYlabeledreduced <- SubjectXYlabeled[,keep.bool]
+     data.table(SubjectXYlabeledreduced)
+     #Update final column names to readable form
+     nameVec <- names(SubjectXYlabeledreduced)
+     newNamesVec <- c("SubjectNum", orderedfeaturesDT$V2, "ActivityCode", "ActivityDesc")
+     setnames(SubjectXYlabeledreduced, nameVec, newNamesVec)
+     #Output the first final data file into CSV
+     write.table(SubjectXYlabeledreduced,"./tidydata/tidydataFull.txt", row.names=FALSE)
+     #Create the second tidy data file
+     SubjectXYlabeledreducedDT <- data.table(SubjectXYlabeledreduced)
+     newsummary <- SubjectXYlabeledreducedDT[, lapply(.SD, mean), by=.(SubjectXYlabeledreducedDT$SubjectNum,SubjectXYlabeledreducedDT$ActivityDesc)]
+     newsummary[,ActivityCode:=NULL]
+     setnames(newsummary, c("SubjectXYlabeledreducedDT","SubjectXYlabeledreducedDT.1"), c("SubjectNum","ActivityDesc"))
+     #Output the second tidy data file into CSV
+     write.table(newsummary,"./tidydata/tidydataAverages.txt", row.names=FALSE)
 }
